ea7106f2cc20006cd2718a4916e61ec9d8cfe994
Aline Dubois <juillet.aline@gmail.com>
2021-04-18 15:35:04 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import QuoteCard from "./components/QuoteCard";
+
 
 function App() {
+  const [quote, setQuote] = useState({
+    "quote": "Gah, stupid sexy Flanders!",
+    "character": "Homer Simpson",
+    "image": "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939",
+    "characterDirection": "Right"
+  });
+
+  function recupererCitation () {
+    fetch("https://simpsons-quotes-api.herokuapp.com/quotes")
+        .then((resp) => resp.json())
+        .then((data) => setQuote(data[0]));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QuoteCard quote={quote.quote} character={quote.character} image={quote.image}/>
+      <button onClick={recupererCitation}>Récuper citation</button>
     </div>
   );
 }
